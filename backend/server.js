4f32fa02241e7ff6ed7a91a3a277253cd9308fbe
Ben <bencamilleri69@gmail.com>
2017-09-30 19:45:20 +0000

    Fixed startup and css loading

--- backend/server.js
+++ backend/server.js
@@ -18,4 +18,4 @@ server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   console.log("Server listening at", addr.address + ":" + addr.port);
 });
 
-router.use(express.static(path.resolve(__dirname, 'client')));
+router.use(express.static(path.resolve(__dirname, '../client')));
