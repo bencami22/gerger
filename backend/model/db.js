ee9f9adc2bc1761d46a59b1795c7112d256fe54d
Ben <bencamilleri69@gmail.com>
2018-03-11 17:13:16 +0000

    live db conn string to get from environment variable

--- backend/model/db.js
+++ backend/model/db.js
@@ -1,35 +1,39 @@
 // Bring Mongoose into the app 
-var mongoose = require('mongoose'); 
+var mongoose = require('mongoose');
 
 // Build the connection string 
-var dbURI = 'mongodb://admin:Hello123!@cluster0-shard-00-00-f9wb8.mongodb.net:27017,cluster0-shard-00-01-f9wb8.mongodb.net:27017,cluster0-shard-00-02-f9wb8.mongodb.net:27017/ComplaintsDB?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'; 
+var dbURI = process.env.dbConnString || 'mongodb://admin:Hello123!@cluster0-shard-00-00-f9wb8.mongodb.net:27017,cluster0-shard-00-01-f9wb8.mongodb.net:27017,cluster0-shard-00-02-f9wb8.mongodb.net:27017/ComplaintsDB?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+
+//LIVE
+//'mongodb://gergeruser:yYLrMA8xKFmy8rlt@gergercluster-shard-00-00-bczjh.mongodb.net:27017,gergercluster-shard-00-01-bczjh.mongodb.net:27017,gergercluster-shard-00-02-bczjh.mongodb.net:27017/ComplaintsDB?ssl=true&replicaSet=gergercluster-shard-0&authSource=admin';
+
 
 // Create the database connection 
-mongoose.connect(dbURI); 
+mongoose.connect(dbURI);
 
 // CONNECTION EVENTS
 // When successfully connected
-mongoose.connection.on('connected', function () {  
+mongoose.connection.on('connected', function() {
   console.log('Mongoose default connection open to ' + dbURI);
-}); 
+});
 
 // If the connection throws an error
-mongoose.connection.on('error',function (err) {  
+mongoose.connection.on('error', function(err) {
   console.log('Mongoose default connection error: ' + err);
-}); 
+});
 
 // When the connection is disconnected
-mongoose.connection.on('disconnected', function () {  
-  console.log('Mongoose default connection disconnected'); 
+mongoose.connection.on('disconnected', function() {
+  console.log('Mongoose default connection disconnected');
 });
 
 // If the Node process ends, close the Mongoose connection 
-process.on('SIGINT', function() {  
-  mongoose.connection.close(function () { 
-    console.log('Mongoose default connection disconnected through app termination'); 
-    process.exit(0); 
-  }); 
-}); 
+process.on('SIGINT', function() {
+  mongoose.connection.close(function() {
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
+});
 
 // BRING IN YOUR SCHEMAS & MODELS // For example 
 require('../model/Complaint');
